cdce4a84a9ea683bcbaed1aa03fc9d62410d19ad
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-03 15:28:28 +0200

    feat: constructed the lists of emails and phone numbers, and added them in the right format to thier respective files, in addition, I also checked that none of them already exist in the existing-contacts.txt file

---
+++ automation/automation.py
@@ -0,0 +1,41 @@
+import re
+
+
+def check_exists(query):
+    with open('assets/existing-contacts.txt', 'r') as e:
+        return query in e.read()
+
+
+with open("assets/potential-contacts.txt", "r") as f:
+    content = f.read()
+
+phone_numbers = list(filter(lambda v: not check_exists(v), list(set([item[0] for item in re.findall(
+    r"((\+?\d{1,3}\W)?(\(?(\d{3})?\)?(-|\s)?)?\d{3}\W?\d{4}(x\d+)?)", content)]))))
+
+new_phone_numbers = []
+for number in phone_numbers:
+    number = re.sub(r"(\+1\W)|(001\W)", "", number)
+    num = ''
+    for n in number:
+        if n == 'x' or len(num) >= 12:
+            break
+        elif n.isdigit():
+            num += n
+            if (len(num) == 3) or (len(num) == 7):
+                num += "-"
+
+    new_phone_numbers.append(num)
+
+emails = sorted(list(set(list(filter(lambda v: not check_exists(v), [
+                item[0] for item in re.findall(r"(\w+@(\w+[-]?)+\.(com|info|net|org|biz))", content)])))))
+with open('phone_numbers.txt', 'a') as p:
+    for num in new_phone_numbers:
+        p.write(num + '\n')
+
+with open('emails.txt', 'a') as e:
+    for email in emails:
+        e.write(email + '\n')
+
+
+#print(re.sub(r"\w-", "@", "h-ello m- an"))
+#phone_numbers = sorted(list(map(lambda num : ''.join([n if n.isdigit() else ""  for i,n in enumerate(re.sub(r"(\+1\W)|(001\W)","",num))]), phone_numbers)))
